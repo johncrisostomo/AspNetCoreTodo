70747aed6f42ff2a017e73501f1dd5822ca95a4f
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-16 09:53:09 +0800

    Adjusts view to support adding todo items

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,20 @@
 ﻿// Write your JavaScript code.
+$(document).ready(function () {
+    $('#add-item-button').on('click', addItem);
+});
+
+function addItem() {
+    $('#add-item-error').hide();
+    let newTitle = $('#add-item-title').val();
+
+    $.post('/Todo/AddItem', { title: newTitle }, function() {
+        window.location = '/Todo';
+    })
+    .fail(function (data) {
+        if (data && data.response.JSON) {
+            let firstError = data.responseJSON[Object.keys(data.responseJSON)[0]];
+            $('#add-item-error').text(firstError);
+            $('#add-item-error').show();
+        }
+    })
+}
