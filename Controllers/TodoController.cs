70ada75af6de892c3a0031cf5530455ecba047c7
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-17 10:19:39 +0800

    Injected userManager in controller

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -5,20 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AspNetCoreTodo.Services;
 using AspNetCoreTodo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace AspNetCoreTodo.Controllers 
 {
+    [Authorize]
     public class TodoController : Controller 
     {
         private readonly ITodoItemService _todoItemService;
-        public TodoController(ITodoItemService todoItemService) 
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public TodoController(ITodoItemService todoItemService, UserManager<ApplicationUser> userManager) 
         {
             _todoItemService = todoItemService;
+            _userManager = userManager;
         }
 
         public async Task<IActionResult> Index() 
         {
-            var  todoItems = await _todoItemService.GetIncompleteItemsAsync();
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser == null) return Challenge();
+
+            var  todoItems = await _todoItemService.GetIncompleteItemsAsync(currentUser);
 
             var model = new TodoViewModel()
             {
